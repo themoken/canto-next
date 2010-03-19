11a09fc001f86d10a079c7ef51393a55b81e15ea
Jack Miller <jack@codezen.org>
2010-03-19 16:06:23 -0500

    Add bad canto-fetch test
    Currently the URL doesn't exist.
    
    Signed-off-by: Jack Miller <jack@codezen.org>

--- tests/canto_fetch.py
+++ tests/canto_fetch.py
@@ -15,7 +15,7 @@ log = logging.getLogger("CF-TEST")
 
 FEED_SHELF = "feed.shelf"
 
-def test():
+def test_good_fetch_test():
     shelf = storage.CantoShelf(FEED_SHELF)
 
     cfg = config.CantoConfig("tests/good/fetch-test.conf", shelf)
@@ -35,8 +35,38 @@ def test():
         log.debug("failed to get feedparser output!")
         return 0
 
-    print "CANTO-FETCH TEST PASSED"
     return 1
+
+def test_bad_fetch_test():
+    shelf = storage.CantoShelf(FEED_SHELF)
+
+    cfg = config.CantoConfig("tests/bad/fetch-test.conf", shelf)
+    cfg.parse()
+
+    fetch = canto_fetch.CantoFetch(shelf, cfg.feeds)
+    fetch.fetch()
+
+    # Make sure only the main "thread" is running.
+    if threading.activeCount() > 1:
+        log.debug("fetch returned, threads still active!")
+        return 0
+
+    feed = cfg.feeds[0]
+    if feed.feedparsed:
+        log.debug("Got feedparser output for bad feed!?")
+        return 0
+
+    return 1
+
+def test():
+    if not test_good_fetch_test():
+        log.debug("FAILED test_good_fetch_test")
+        return
+    if not test_bad_fetch_test():
+        log.debug("FAILED test_bad_fetch_test")
+        return
+
+    print "CANTO-FETCH TESTS PASSED"
 
 def cleanup():
     pass
