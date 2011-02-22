1fb82beae3ab296cf56f312dea042971fb4f2e78
Jack Miller <jack@codezen.org>
2011-02-22 16:28:59 -0600

    Cleanup .canto_socket in tests
    Signed-off-by: Jack Miller <jack@codezen.org>

--- tests/clean.sh
+++ tests/clean.sh
@@ -4,6 +4,7 @@
 # Delete everything but the skel files.
 
 for test_dir in "$@"; do
+    rm -f ./$test_dir/.canto_socket
     for f in ./$test_dir/*; do
         if [ "$f" != "./$test_dir/skel" ]; then
             rm -rf "$f"
--- tests/setuptest.sh
+++ tests/setuptest.sh
@@ -12,4 +12,8 @@ for test_dir in "$@"; do
     # Start daemon there.
 
     canto-daemon -v -D "./$test_dir" &
+
+    while [ ! -e "./$test_dir/.canto_socket" ]; do
+        sleep 0.1
+    done
 done
